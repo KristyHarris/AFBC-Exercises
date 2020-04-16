a1bdee42d73d8dd63663f6eacc659fa984b54800
Kristy <harrisk26@gmail.com>
2020-04-15 22:07:25 -0400

    Adds exercise 6

---
+++ Exercise6/Exercise 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            string response = "";
+            do
+            {
+                Console.WriteLine("Hello World!"); 
+                Console.WriteLine("Would you like to continue? (y/n)");
+                response = Console.ReadLine();
+            } while(response == "y");
+           
+        }
+    }
+}
