0af1a9781f808e21eb4ceca4663db8bcd44ec4d5
Kristy <harrisk26@gmail.com>
2020-04-20 16:36:32 -0400

    second commit

---
+++ Exercise7/Exercise7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        string response = "";
+            do
+            {
+                Console.WriteLine("Hello World!"); 
+                Console.WriteLine("Would you like to continue? (y/n)");
+                response = Console.ReadLine();
+            } while(response == "y");
+    }
+}
