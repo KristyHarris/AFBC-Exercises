f3f7bc5aa01b46664892fcb3640e06c573386b5a
Kristy <harrisk26@gmail.com>
2020-04-20 16:59:55 -0400

    adds class 3 and Exercise 7

---
+++ Class 3/Class 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Class_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            do
+            {
+                Console.WriteLine("Enter a number");
+                number = Console.ReadLine();
+                var num = int.Parse(number);
+
+                for (int i = 0; i <= num; i++)
+               {
+                 Console.WriteLine(i);
+               }
+                Console.WriteLine("Would you like to continue? y/n");
+                response = Console.ReadLine();
+            } while (response == "y");
+
+            Console.WriteLine("Goodbye!");
+
+         
+       
+        }
+    }
+}
+
